0ef3d046919fabf21f5b66bc71fa6e66d27f022c
Rafael Reyes <rafaelalexanderr5@gmail.com>
2019-12-02 22:24:29 -0600

    Working on the banner - miss the cover letter

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import vector from './images/laptop-phone.png'
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="Header">
       <div className="content-logo">Rarepz</div>
+      <div className="content-vector">
+        <img src={vector} alt="vector of the header"/>
+      </div>
       <div className="content-description">
         <h1>
           <span>rafael alexander reyes perez</span>
