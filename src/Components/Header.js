ca8d2cd2412a36e04f84dddb3722d11bf688a782
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-01-07 15:41:05 -0400

    ADD info header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,20 +1,32 @@
 import React from "react";
-import vector from './images/laptop-phone.png'
+import shadow from './images/shadow.svg';
+import vector from "./images/vector-header.svg";
 import "./Header.css";
 
 const Header = () => {
+  let email = 'mailto:rafaelalexanderr5@gmail.com'
+  let phone = 'tel:8294302840'
   return (
     <div className="Header">
-      <div className="content-logo">Rarepz</div>
+      <div className="content-logo">
+        <div className="shadow">
+        <img src={shadow} alt="vector of the header" />
+          <h2>Rarepz</h2>
+        </div>
+      </div>
       <div className="content-vector">
-        <img src={vector} alt="vector of the header"/>
+        <img src={vector} alt="vector of the header" />
       </div>
       <div className="content-description">
         <h1>
           <span>rafael alexander reyes perez</span>
           Front-end Developer
         </h1>
+        <p><a href={phone}>+1 (829) 430-2840</a></p>
+        <p><a href={email}>rafaelalexanderr5@gmail.com</a></p>
+        <p><a href={email}>rafaelalexanderr5@gmail.com</a></p>
       </div>
+      <div className="skew-effect"></div>
     </div>
   );
 };
