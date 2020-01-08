24d68142bdf30a61ee88d96654501484950886d0
Rafael Reyes <r.reyes@caribemedia.com.do>
2020-01-08 17:53:19 -0400

    ADD - Header wave

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import shadow from './Header/images/shadow.svg';
+import shadow from './images/shadow.svg';
 // import vector from "./images/vector-header.svg";
 import "./Header.css";
 
@@ -19,11 +19,17 @@ const Header = () => {
           <span>rafael alexander reyes perez</span>
           Front-end Developer
         </h1>
-        
          <div className="shadow">
         <img src={shadow} alt="vector of the header" />
         </div><p><a href={phone}>+1 (829) 430-2840</a></p>
         <p><a href={email}>rafaelalexanderr5@gmail.com</a></p>
+      </div>
+      <div className="wave-header">
+        <div className="style-wave">
+          <svg viewBox="0 0 500 150" preserveAspectRatio="none" className="style-wave2">
+            <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 556.99,117.92 L500.00,150.00 L0.00,150.00 Z" className="style-wave3"></path>
+          </svg>
+        </div>
       </div>
     </div>
   );
