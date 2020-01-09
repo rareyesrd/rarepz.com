eac11a1fdcc1930023b1d3b61c5a6e32de11106b
Rafael Reyes <rafaelalexanderr5@gmail.com>
2020-01-08 21:55:25 -0600

    Finish - the header component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -16,22 +16,19 @@ const Header = () => {
       </div>
       <div className="content-description">
           <h1>
-          <span>rafael alexander reyes perez</span>
+          <span>rafael alexander reyes pérez</span>
           Front-end Developer
         </h1>
          <div className="shadow">
-        <img src={shadow} alt="vector of the header" />
+        {/* <img src={shadow} alt="vector of the header" /> */}
         </div><p><a href={phone}>+1 (829) 430-2840</a></p>
         <p><a href={email}>rafaelalexanderr5@gmail.com</a></p>
       </div>
-      <div className="wave-header">
-        <div className="style-wave">
-          <svg viewBox="0 0 500 150" preserveAspectRatio="none" className="style-wave2">
-            <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 556.99,117.92 L500.00,150.00 L0.00,150.00 Z" className="style-wave3"></path>
-          </svg>
-        </div>
+      <div className="wave wave1"></div>
+      <div className="wave wave2"></div>
+      <div className="wave wave3"></div>
+      <div className="wave wave4"></div>
       </div>
-    </div>
   );
 };
 
